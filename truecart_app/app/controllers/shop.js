3324718c8ae2db06aefcfcb4f8fe55b981bc3d1d
Ethan Heckard <eheckard23@gmail.com>
2017-05-24 17:14:20 -0400

    output database data working

--- truecart_app/app/controllers/shop.js
+++ truecart_app/app/controllers/shop.js
@@ -9,6 +9,16 @@ module.exports = (app) => {
 
 router.get('/', (req,res,next) => {
 
-	res.render(path.join( __dirname, '/../views/shop'), { title: 'TrueCart' });
+	let Product = mongoose.model('Product');
+
+	Product.find({}, (err, products) => {
+		if(err) throw err;
+		let productChunks = [];
+		let chunkSize = 3;
+		for(let i=0;i<products.length;i+=chunkSize){
+			productChunks.push(products.slice(i, i+chunkSize));
+		}
+		res.render(path.join( __dirname, '/../views/index'), { title: 'TrueCart', products: productChunks });
+	});
 
 });
---
+++ truecart_app/app/seeds/product-seeds.js
@@ -0,0 +1,61 @@
+let newProduct = new Product({
+		img: 'http://images.gibson.com.s3.amazonaws.com/Products/Electric-Guitars/2016/Custom/CC35-Vic-DaPra-1959-LP-Gruhn-Burst/LP59CC35TSBNH1_ELECTRONICS_GLAM.jpg',
+		title: 'Gison Les Paul',
+		description: 'Precise replica, handcrafted, Double-carved top/neck',
+		price: 10499
+	});
+
+	let newProduct2 = new Product({
+		img: 'http://images.gibson.com.s3.amazonaws.com/Products/Electric-Guitars/2016/Custom/SG-Standard-VOS/SGSR4VOFCNH1_ELECTRONICS_GLAM.jpg',
+		title: 'Gison SG Standard',
+		description: 'The 1961 "Les Paul" That Wasnt to Be',
+		price: 4499
+	});
+	let newProduct3 = new Product({
+		img: 'http://images.gibson.com.s3.amazonaws.com/Products/Electric-Guitars/2016/Custom/Flying-V-Standard/CSFVSITNH1_ELECTRONICS_GLAM.jpg',
+		title: 'Gison Flying V Standard',
+		description: 'Figured Maple Top, Solid Mahogany Body',
+		price: 5299
+	});
+	let newProduct4 = new Product({
+		img: 'https://reverb-res.cloudinary.com/image/upload/s--_dhidtpN--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_620,q_90,w_620/v1472510878/g7e1xukgkphzqwta5bvu.jpg',
+		title: 'Tama 4-Piece Superstar Classic Jazz Shell Kit (Midnight Gold Sparkle)',
+		description: 'Midnight Gold Sparkle',
+		price: 649
+	});
+	let newProduct5 = new Product({
+		img: 'https://static1.squarespace.com/static/57cebe2c03596e075fca5f24/580a66771b631b3a17b6b1bc/583a1e07e4fcb5b7cf393393/1480203787192/Palisades.jpg?format=500w',
+		title: 'Earthquaker Palisades Overdrive',
+		description: 'Mega Ultimate Overdrive',
+		price: 249
+	});
+	let newProduct6 = new Product({
+		img: 'https://reverb-res.cloudinary.com/image/upload/s--THlRfdZE--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_620,q_90,w_620/v1495533956/azpl7pbc7vrcm8scvohg.jpg',
+		title: 'Martin D28 1965',
+		description: '1965 Model',
+		price: 8730
+	});
+	let newProduct7 = new Product({
+		img: 'https://reverb-res.cloudinary.com/image/upload/s--kTApAsfF--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_1600,q_80,w_1600/v1448060640/j3am6ty9oeeikiuxd1aa.jpg',
+		title: 'Kala Guitarlele Koa 6-String',
+		description: 'Solid Spruce Top',
+		price: 300
+	});
+	let newProduct8 = new Product({
+		img: 'https://reverb-res.cloudinary.com/image/upload/s--H_YfozfY--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_620,q_90,w_620/v1471113039/oqvqlnez5gjgvo8axf1j.jpg',
+		title: 'Moog MiniMoog Model D',
+		description: 'Natural Wood Analog Synth',
+		price: 4495
+	});
+	let newProduct9 = new Product({
+		img: 'https://reverb-res.cloudinary.com/image/upload/s--utLyUzp1--/c_crop,h_1.000,w_0.693,x_0.149,y_0.000/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_620,q_90,w_620/v1488339130/zzzrjmg8qwibtosh6jrh.jpg',
+		title: 'Fender Rhodes Mark II Stage 73 Keyboard 1980',
+		description: 'Black Tolex',
+		price: 2300
+	});
+	let newProduct10 = new Product({
+		img: 'https://reverb-res.cloudinary.com/image/upload/s--UiD5lJfv--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_620,q_90,w_620/v1449611037/rf8ofcgue6jufldbwg00.jpg',
+		title: 'Korg MicroKEY249 49-Mini Key USB MIDI Keyboard',
+		description: 'USB MIDI keyboard – thin, lightweight, and USB-powered',
+		price: 120
+	});
